3368e315f6464957fb5d07dedfb5e8514fef293b
yagiz <yagocevat123@gmail.com>
2026-06-20 22:08:27 +0300

    Added backend

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,20 +1,18 @@
-import { Routes, Route } from 'react-router';
-import './App.css'
-import { HomePage } from './pages/HomePage'
-import { CheckoutPage } from './pages/CheckoutPage';
-import { OrdersPage } from './pages/OrdersPage';
-import { TrackingPage } from './pages/TrackingPage';
+import { Routes, Route } from "react-router";
+import "./App.css";
+import { HomePage } from "./pages/HomePage";
+import { CheckoutPage } from "./pages/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
 function App() {
-
   return (
-     
-     <Routes>
-        <Route index element={<HomePage />}/>
-        <Route path= "checkout" element={<CheckoutPage />}/>
-        <Route path='orders' element={<OrdersPage />} />
-        <Route path='tracking' element={<TrackingPage />}/>
-     </Routes>
-  )
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
