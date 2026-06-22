ef9f652c5fc97d78256bdb4fb4ab1167ffaa3a64
yagiz <yagocevat123@gmail.com>
2026-06-22 16:34:21 +0300

    Create add to cart feature

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -8,19 +8,19 @@ import "./App.css";
 
 function App() {
   const [cart, setCart] = useState([]);
-
-  useEffect(() => {
-    const fetchAppData = async () => {
+  
+  const loadCart = async () => {
       const response = await axios.get("/api/cart-items?expand=product");
       setCart(response.data);
     };
 
-    fetchAppData();
+  useEffect(() => {
+    loadCart();
   }, []);
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart} />} />
+      <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
     </Routes>
